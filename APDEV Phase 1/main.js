e865065ceefb35d6c39cbcfe6d318349456d666a
Tweetsauceyeep <pobusauce12@gmail.com>
2026-02-09 21:38:41 +0800

    Shows current logged in profile in homepage

--- APDEV Phase 1/main.js
+++ APDEV Phase 1/main.js
@@ -9,6 +9,11 @@ if (!storedUser) {
 
 const currentUser = JSON.parse(storedUser);
 
+if (storedUser) {
+  const user = JSON.parse(storedUser);
+  document.getElementById("userInfo").textContent = `Logged in as: ${user.name} (${user.role})`;
+}
+
 function logout() {
   localStorage.removeItem("currentUser");
   sessionStorage.removeItem("currentUser");
